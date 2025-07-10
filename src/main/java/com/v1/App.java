9fb54fa4811945b5d5890e8df2440e7a762b08cd
soo59599 <sooh59599@gmail.com>
2025-07-10 14:21:34 +0900

    com.v1 패키지 숫자 입력 예외처리 연산자 예외처리 char 타입으로 변경 exit 대소문자 구별없음으로 변경 커밋

--- src/main/java/com/v1/App.java
+++ src/main/java/com/v1/App.java
@@ -1,5 +1,6 @@
 package com.v1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -10,21 +11,38 @@ public class App {
         while (true) {
             //결과값 저장 변수 설정
             double answer = 0;
+            int num1 =0; int num2 = 0;
 
-            //첫 숫자 입력
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int num1 = sc.nextInt();
+            //첫 숫자 입력 및 예외처리
+            while (true) {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                try {
+                    num1 = sc.nextInt();
+                    break;
+                }catch (InputMismatchException e){
+                    System.out.println("숫자만 입력 가능합니다.");
+                    sc.next();
+                }
+            }
 
             //두번째 숫자 입력
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int num2 = sc.nextInt();
+            while (true) {
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                try {
+                    num2 = sc.nextInt();
+                    break;
+                }catch (InputMismatchException e){
+                    System.out.println("숫자만 입력 가능합니다.");
+                    sc.next();
+                }
+            }
 
             //연산자 입력
             System.out.println("사칙연산 기호(+,-,/,*)를 입력하세요: ");
             char operator = sc.next().charAt(0);
 
             //연산자 예외처리(set 활용)
-            Set<String> operators = Set.of("+", "-", "*", "/");
+            Set<Character> operators = Set.of('+', '-', '*', '/');
             while (!operators.contains(operator)) {
                 System.out.println("유효하지 않은 연산자입니다.");
                 System.out.println("연산자(+,-,/,*)중에 한가지를 입력하세요.");
@@ -66,12 +84,12 @@ public class App {
             System.out.println("계산을 계속하려면 아무버튼이나 입력하세요.");
             System.out.println("계산을 그만두려면 exit를 입력하세요.");
             String str = sc.next();
-            if ("exit".equals(str)) {
+            if ("exit".equalsIgnoreCase(str)) {
                 break;
             }
 
         }
-
+        sc.close();
     }
 
 }
